3b9cdee7a4462bb77da329134f954178190a8134
s4mmeliu <167963780+s4mmeliu@users.noreply.github.com>
2024-04-25 10:20:30 +0300

    Add files via upload

---
+++ Noppa_Heita_Vepsäläinen/Noppa_Heita/Noppa_Heita/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Noppa_Heita
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_noppa1_luvut = new System.Windows.Forms.TextBox();
+            this.txt_noppa2_luvut = new System.Windows.Forms.TextBox();
+            this.txt_yhteensa = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(87, 263);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_noppa1_luvut
+            // 
+            this.txt_noppa1_luvut.Location = new System.Drawing.Point(233, 228);
+            this.txt_noppa1_luvut.Name = "txt_noppa1_luvut";
+            this.txt_noppa1_luvut.Size = new System.Drawing.Size(100, 26);
+            this.txt_noppa1_luvut.TabIndex = 1;
+            // 
+            // txt_noppa2_luvut
+            // 
+            this.txt_noppa2_luvut.Location = new System.Drawing.Point(231, 285);
+            this.txt_noppa2_luvut.Name = "txt_noppa2_luvut";
+            this.txt_noppa2_luvut.Size = new System.Drawing.Size(102, 26);
+            this.txt_noppa2_luvut.TabIndex = 2;
+            // 
+            // txt_yhteensa
+            // 
+            this.txt_yhteensa.Location = new System.Drawing.Point(454, 269);
+            this.txt_yhteensa.Name = "txt_yhteensa";
+            this.txt_yhteensa.Size = new System.Drawing.Size(98, 26);
+            this.txt_yhteensa.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_yhteensa);
+            this.Controls.Add(this.txt_noppa2_luvut);
+            this.Controls.Add(this.txt_noppa1_luvut);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_noppa1_luvut;
+        private System.Windows.Forms.TextBox txt_noppa2_luvut;
+        private System.Windows.Forms.TextBox txt_yhteensa;
+    }
+}
+
---
+++ Noppa_Heita_Vepsäläinen/Noppa_Heita/Noppa_Heita/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Noppa_Heita
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int[] numerot = new int[5];
+            for (int i = 0; i < 5; i++) 
+            {
+                int NopanNumero = random.Next(1, 7);
+                numerot[i] = NopanNumero;
+            }
+            int[] noppa1 = numerot;
+            txt_noppa1_luvut.Clear();
+            Array.Sort(numerot);
+            foreach (var numero in noppa1) { txt_noppa1_luvut.AppendText($"{numero} {Environment.NewLine}"); }
+
+            int[] numerot2 = new int[5];
+            for (int j = 0; j < 5; j++)
+            {
+                int NopanNumero2 = random.Next(1, 7);
+                numerot2[j] = NopanNumero2;
+            }
+            int[] noppa2 = numerot2;
+            txt_noppa2_luvut.Clear();
+            Array.Sort(numerot2);
+            foreach (var numero2 in noppa2) { txt_noppa2_luvut.AppendText($"{numero2} {Environment.NewLine}"); }
+            txt_yhteensa.Clear();
+
+            int noppien_numerot = 0;
+            foreach (int numero1 in noppa1)
+            {
+                noppien_numerot += numero1;
+            }
+            foreach (int numero2 in noppa2)
+            {
+            noppien_numerot += numero2;
+            }
+            txt_yhteensa.AppendText($"{noppien_numerot}");
+        }
+    }
+}
